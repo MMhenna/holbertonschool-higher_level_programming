4618eda1fa1fa60ac088ba2eb8bd40636f695b7f
MMhenna <mhenna.mehdi@gmail.com>
2021-05-06 19:13:03 +0100

    fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 
-def square_nums(num):
-    return num * num
-
 
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
-    for num in matrix:
-        new_matrix.append(list(map(square_nums, num)))
-
-    return new_matrix
+    if (matrix):
+        new_matrix = []
+        for row in matrix:
+            new_matrix.append(list(map(lambda x: x * x, row)))
+        return (new_matrix)
