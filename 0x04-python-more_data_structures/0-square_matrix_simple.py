e18372f7b655108017c4e825a6ffe48f7812fb95
MMhenna <mhenna.mehdi@gmail.com>
2021-05-06 13:24:07 +0100

    co

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def square_nums(num):
+    return num * num
+
+
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for num in matrix:
+        new_matrix.append(list(map(square_nums, num)))
+
+    return new_matrix
