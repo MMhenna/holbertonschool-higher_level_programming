dd551b7f7905f259bf84b84e6afc290d3cde1e2e
MMhenna <mhenna.mehdi@gmail.com>
2021-05-06 19:16:12 +0100

    fix pep8

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
-
 def uniq_add(my_list=[]):
-return sum(list(set(my_list))) 
+    return sum(list(set(my_list))) 
--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 def print_sorted_dictionary(a_dictionary):
     if a_dictionary:
         sorted_keys = sorted(a_dictionary.keys())
