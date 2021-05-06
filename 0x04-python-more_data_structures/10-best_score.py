74c9f4e49fa38c8a37838cbf913838701e56a285
MMhenna <mhenna.mehdi@gmail.com>
2021-05-06 20:21:01 +0100

    fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def best_score(my_dict):
-    if my_dict:
-        return (max(my_dict.value()))
+    if not my_dict:
+        return None
+    
+    for i in my_dict:
+        if my_dict[i] == max(my_dict.values()):
+
+    return i
