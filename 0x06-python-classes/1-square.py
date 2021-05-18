2c4996bc2fe0ad325e9eeeffd6f9016bda13317d
MMhenna <mhenna.mehdi@gmail.com>
2021-05-18 16:10:32 +0100

    fix 3

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,10 +2,6 @@
 
 
 class Square:
-    """Defining a Square"""
+    """Creates an object from the Square class"""
     def __init__(self, size=None):
-        """ Initialization of instance attributes
-            Args:
-            size (int): Zero or positve number.
-        """
         self.__size = size
