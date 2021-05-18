b78eec775a998b65bc5e28856f440bbc4690f4db
MMhenna <mhenna.mehdi@gmail.com>
2021-05-18 16:49:26 +0100

    fix 5

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
 
-"""1-square.py: class Square"""
+"""2-square.py: class Square"""
 
 class Square:
     """Represents a square with Private instance attribute size.
         Attribute:
-            size (int): size of suare
+            size (int): size of square.
     """
     
     def __init__(self, size=0):
         """Initialization of instance attributes.
-            Args:
-                size (int): Zero or positve number.
+
+        Args:
+            size (int): Zero or positve number.
+        Raises:
+            TypeError: If size not int.
+            ValueError: If size < 0.
         """
         if type(size) is not int:
             raise TypeError("size must be an integer")
