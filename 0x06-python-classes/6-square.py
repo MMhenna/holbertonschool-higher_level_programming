87a593bf94a9cffe7bac0112322cf56ab2f20f56
MMhenna <mhenna.mehdi@gmail.com>
2021-05-18 17:50:35 +0100

    fix pep8

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,78 +1,69 @@
 #!/usr/bin/python3
+""" This module is used as intro to classes """
 
 
-class Square:    
-
-    """Defining a Square"""
-
-    def __init__(self, size=None, position=(0, 0)):
-        '''Initialization of instance attributes
-            Args: 
-            size (int): Zero or positve number.
-            position (int): 2 coordinate position.
-        '''
-        if type(size) is not int:
+class Square:
+    """ square class with size(>=0 integer) attribute """
+    def __init__(self, size=0, position=(0, 0)):
+        if type(size) is int:
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
             raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        elif (len(position) is not 2 or type(position[0]) is not int or type(position[1]) is not int):
+
+        if (
+                type(position) is not tuple or
+                len(position) != 2 or
+                any(map(lambda x: type(x) is not int or x < 0, position))
+                ):
             raise TypeError("position must be a tuple of 2 positive integers")
-        elif position[0] < 0 or position[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        else
-            self.__size = size
+        else:
             self.__position = position
 
     @property
     def size(self):
-        """returns the size of the length of the square"""
+        """ size attibute getter """
         return self.__size
 
     @size.setter
     def size(self, value):
-        '''sets the size value
-            Args:
-            value (int): Zero or positve number.
-        '''
-        if type(value) is not int:
+        """ size attribute setter """
+        if type(value) is int:
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
             raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        else
-            self.__size = value
 
     @property
-    def size(self):
-        """returns the position"""
+    def position(self):
+        """ position attribute gerrer """
         return self.__position
 
     @position.setter
-    def size(self, value):
-        """sets the position values
-            Args:
-            value (int): the tuple value to set the position
-        """
-        if (len(position) is not 2 or type(position[0]) is not int or type(position[1]) is not int):
+    def position(self, value):
+        if (
+                type(position) is not tuple or
+                len(position) != 2 or
+                any(map(lambda x: type(x) is not int or x < 0, position))
+                ):
             raise TypeError("position must be a tuple of 2 positive integers")
-        elif position[0] < 0 or position[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        else
+        else:
             self.__position = position
 
     def area(self):
-        '''Calculates the area
-            Return: 
-            The current square area.
-        '''
-        return self.__size * self.__size
+        """ calculate and return the area of the square """
+        return self.__size ** 2
 
     def my_print(self):
-         """Prints the a square in # the size of the square"""
+        """ draw the square on stdout using the '#' character """
         if self.__size == 0:
             print()
         else:
-            for i in range(self.__size):
-                print("#" * self.__size)
-
-
-    
+            print('\n' * self.__position[1], end='')
+        for i in range(self.__size):
+            print(' ' * self.__position[0], end='')
+            print('#' * self.__size)
