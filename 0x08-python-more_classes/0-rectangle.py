0faf9da2c4e1592ffadb83b0e29a331e6f41f4d1
MMhenna <mhenna.mehdi@gmail.com>
2021-05-24 22:17:05 +0100

    fix pep8

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-"""0-rectangle.py: module for an empty Rectangle class """
+"""0-rectangle.py: module for an empty Rectangle class"""
 
 class Rectangle:
     """Rectangle class"""
