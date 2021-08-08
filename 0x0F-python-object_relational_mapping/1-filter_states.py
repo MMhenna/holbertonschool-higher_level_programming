303d3ee429b37ad7131314a1c4134ae51dfcd40c
MMhenna <mhenna.mehdi@gmail.com>
2021-08-08 23:57:30 +0100

    commit

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-'''Lists all states with a name starting with N from the      
-database'''             
+''' Lists all states with a name starting with N from the database
+'''             
 import sys
 import MySQLdb
 
